920c1c305e463cd0b76cd0ac6b9ced27c6633b67
bferi2001 <barnafeco@gmail.com>
2024-09-02 14:45:24 +0200

    lilla

--- data_providers/jira_utility.py
+++ data_providers/jira_utility.py
@@ -1,17 +1,31 @@
 from requests.auth import HTTPBasicAuth
-import requests
+import requests, json
+import decorators
 
 class JiraUtility:
     
     def __init__(self, user, jira_pat) -> None:
         self.url = 'https://barnafeco.atlassian.net/rest/api/2/search?jql='
-        self.filter=''
-        auth = HTTPBasicAuth(user, jira_pat)
-        print(f"{self.url}{self.filter}")
-        with open("asda.json", "w", encoding="utf-8") as file:
-            response = requests.get(
-                    f"{self.url}{self.filter}", 
+        self.auth = HTTPBasicAuth(user, jira_pat)
+        self.getJiraTickets()
+        
+    
+        
+    @decorators.return_json_to_file 
+    def getJiraTickets(self) -> None:
+        x = 0
+        total_tickets = None
+        while(not total_tickets or (x*50<total_tickets)):
+            response = json.loads(
+            requests.get(
+                    f"{self.url}", 
                     headers={"Accept": "application/json"},
-                    auth=auth
+                    auth=self.auth
                     ).text
-            file.write(str(response))
+            )
+            total_tickets=response["total"]
+            x+=1
+        return response
+            
+        
+    
