54fac1455bf8e3748b042c541600938bc55e601c
bferi2001 <barnafeco@gmail.com>
2024-09-10 17:11:05 +0200

    JTMD-10 Markdowns created with the right tickets

--- data_providers/jira_utility.py
+++ data_providers/jira_utility.py
@@ -26,15 +26,15 @@ class JiraUtility:
             x+=1
         return all_tickets_details
     
-    def get_story_tickets(self) -> list:
-        return self.get_tickets("type=Story")
+    def get_story_tickets_with_path(self) -> list:
+        return self.get_tickets('type=Story AND "Path[Labels]"!=NULL')
     
     @decorators.return_json_to_file
     def get_stories_to_path(self, path:str)->list:
-        tickets=self.get_story_tickets()
+        tickets=self.get_story_tickets_with_path()
         tickets_with_good_path=list()
         for ticket in tickets:
             if ticket["fields"]["customfield_10143"]:
-                if any(e.startswith(path) for e in ticket["fields"]["customfield_10143"]):
+                if any(e.replace("/", "\\").startswith(path) for e in ticket["fields"]["customfield_10143"]):
                     tickets_with_good_path.append(ticket)
         return tickets_with_good_path
--- helpers/markdown.py
+++ helpers/markdown.py
@@ -19,7 +19,8 @@ def table_from_matrix(matrix) -> None:
 
 
 def create_ticketlist_markdown_to_path(path:str, jira:JU)->None:
-    tickets_details=jira.get_stories_to_path(path)
-    tickets_matrix=ttmd.tickets_details_to_matrix(tickets_details)
+    relpath=os.path.relpath(path, os.getcwd())
+    tickets_details=jira.get_stories_to_path(relpath)
+    tickets_matrix=ttmd.tickets_details_to_matrix(tickets_details)    
     table=table_from_matrix(tickets_matrix)
     create_markdown(path=path, content=table)
